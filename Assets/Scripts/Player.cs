577e7a7c535f025a06d67c82b50f836493477fa9
GerardCaja <120013160+GerardCaja@users.noreply.github.com>
2023-10-11 10:28:57 +0200

    afasd

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class Player : MonoBehaviour
 {
@@ -14,14 +15,14 @@ public class Player : MonoBehaviour
 
     private Rigidbody2D _rBody2D; 
     //private GroundSensor _sensor;
-    private Animator _animator;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private PlayableDirector _director;
 
     // Start is called before the first frame update
     void Start()
     {
         _rBody2D = GetComponent<Rigidbody2D>();
         //_sensor = GetComponentInChildren<GroundSensor>();
-        _animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -32,6 +33,11 @@ public class Player : MonoBehaviour
         if(Input.GetButtonDown("Jump") && GroundSensor._isGrounded)
         {
             Jump();
+        }
+
+        if(Input.GetButtonDown("Fire2"))
+        {
+            _director.Play();
         }
     }
 
